2e2c2e66dc69ebea78112f1e136a3d869034bb3f
ChiWoo-Shin <shin8037@naver.com>
2023-05-13 01:26:02 +0900

    fix: hydrate error 해결 - _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,61 @@
 /* eslint-disable */
+// import '@/styles/globals.css';
+// import type { AppProps } from 'next/app';
+// import Layout from '@/components/Layout';
+// import { useEffect, useState } from 'react';
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   const pathname = typeof window !== 'undefined' ? window.location.pathname : '';
+//   const [isMounted, setIsMounted] = useState(false)
+
+//   useEffect(() => setIsMounted(true), [])
+
+//   if (pathname === '/' || pathname.startsWith('/signup')) {
+//     // 첫 페이지에서는 레이아웃을 표시하지 않음
+//     return <Component {...pageProps} />;
+//   }
+//   return (
+//     <>
+//     {isMounted &&
+//       <Layout>
+//         <Component {...pageProps} />
+//       </Layout>
+//     }
+//     </>
+//   );
+// }
+
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const pathname = typeof window !== 'undefined' ? window.location.pathname : '';
-  const [isMounted, setIsMounted] = useState(false)
+  const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => setIsMounted(true), [])
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-  if (pathname === '/' || pathname.startsWith('/signup')) {
-    // 첫 페이지에서는 레이아웃을 표시하지 않음
+  if (typeof window !== 'undefined') {
+    const pathname = window.location.pathname;
+
+    if (pathname === '/' || pathname.startsWith('/signup')) {
+      // 첫 페이지에서는 레이아웃을 표시하지 않음
+      return <Component {...pageProps} />;
+    }
+  } else {
+    // 서버 사이드 렌더링일 경우 레이아웃을 표시하지 않음
     return <Component {...pageProps} />;
   }
+
   return (
     <>
-    {isMounted &&
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    }
+      {isMounted && (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </>
   );
 }
